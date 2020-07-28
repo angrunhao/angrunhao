b77731eb771052aecf4d4ce0d44c32d9fc20bf39
Ang Run Hao <angrunhao@gmail.com>
2020-07-28 11:37:45 +0800

    Update Dash app dependencies
    Change Flask Application Factory to modular structure using Werkzeug Dispatcher Middleware

--- angrunhao/__init__.py
+++ angrunhao/__init__.py
@@ -1,15 +1,8 @@
 from flask import Flask
 
-def create_app():
-    #Flask app with embedded Dash
-    app = Flask(__name__, instance_relative_config = False)
-    app.config.from_object("config.Config")
+flask_app = Flask(__name__, instance_relative_config = False)
+flask_app.config.from_object("config.Config")
 
-    with app.app_context():
-        #import routes
-        from . import routes
-        #import Dash app
-        #from .bootstrapping_treasury_zero_curve import create_bootstrapping_treasury_zero_curve
-        #app = create_bootstrapping_treasury_zero_curve(app)
-
-        return app
+with flask_app.app_context():
+    #import routes
+    from . import routes
