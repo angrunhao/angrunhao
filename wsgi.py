1cfc59e705bb97c96dbd9bc1f77b5d6b1db2c587
Ang Run Hao <angrunhao@gmail.com>
2020-07-28 11:41:29 +0800

    Add WSGI production method
    Update procfile to enable running of dispatcher

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.serving import run_simple
+#from werkzeug.serving import run_simple
 
 from angrunhao import flask_app
 from angrunhao.bootstrapping_treasury_zero_curve import btzc_app
@@ -8,6 +8,6 @@ application = DispatcherMiddleware(flask_app, {
     "/btzc_app": btzc_app.server
 })
 
-if __name__ == "__main__":
-    run_simple('localhost', 8050, application,
-               use_reloader=True, use_debugger=True, use_evalex=True)
+#if __name__ == "__main__":
+#    run_simple('localhost', 8050, application,
+#               use_reloader=True, use_debugger=True, use_evalex=True)
