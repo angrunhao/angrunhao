accdc8bba244e5bac8f20cd66faf7090d005161c
Ang Run Hao <angrunhao@gmail.com>
2020-07-29 15:51:53 +0800

    Alter wsgi.py local pipeline

--- wsgi.py
+++ wsgi.py
@@ -9,5 +9,4 @@ application = DispatcherMiddleware(flask_app, {
 })
 
 #if __name__ == "__main__":
-#    run_simple('localhost', 8050, application,
-#               use_reloader=True, use_debugger=True, use_evalex=True)
+#    run_simple('localhost', 8050, application)
